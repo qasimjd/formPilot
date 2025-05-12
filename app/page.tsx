732e937d2f0830951a1b8b15cc7d8c082b6b3760
Qasim Ali <qasimaliwork65@gmail.com>
2025-05-12 19:54:44 +0500

    Initialize Next.js application

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>This is my 1st SAS...</h1>
+    </div>
+  )
+}
+
+export default HomePage
